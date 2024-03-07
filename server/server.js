f204580e009d25b1f6f9c6a1872a618982b0893d
Madhav Agrawal <madhavagrawal012@gmail.com>
2024-03-07 22:19:45 +0530

    added Backend Model and routes

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+const cors = require('cors')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '5mb' })); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+mongoose.connect('mongodb://localhost:27017/Dsafe',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+var db = mongoose.connection;
+db.on('error', () => console.log("error in connecting database"));
+db.once('open', () => console.log("Connected to Database"));
+
+//Signup API starts 
+const SignupApi = require('../server/router/Signup')
+app.use('/api/signup' ,SignupApi )
+
+// code for singupapi ends
+
+// code for loginapi starts
+const LoginApi = require('../server/router/Login')
+app.use('/api/login' , LoginApi)
+//code for login api ends
+
+// api for uploading the file
+const Moralis = require('moralis').default
+Moralis.start({
+    apiKey: "WLoq4IR3FE2QVbSi5cKGXz9gjYuwUeNeIEtJH5f6hWl9cBi5NIM5YV8cTk01IDCl"
+});
+const fileupload = require ('../server/router/UploadFile')
+app.use('/api/uploadfile' , fileupload)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
