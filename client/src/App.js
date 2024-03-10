24f1f16ab6654ccd645ddbf1a845c6e46de0fd9f
Madhav Agrawal <madhavagrawal012@gmail.com>
2024-03-11 02:09:54 +0530

    added tailwinf config

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import Complaint from "./components/Complaint";
 import Credits from "./components/Credits";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
@@ -7,20 +8,25 @@ import Section3 from "./components/Section3";
 import Section4 from "./components/Section4";
 import Section5 from "./components/Section5";
 import Section6 from "./components/Section6";
-
+import Profile  from "./components/Profile"
 function App() {
   return (
-    <div classNameName="App">
-        <Navbar/>
-        <Section1/>
-        <Section2/>
-        <Section3/>
-        <Section4/>
-        <Section5/>
-        <Section6/>
-        <Footer/>
-        <Credits/>
-    </div>
+    <>
+      {/* <Complaint /> */}
+      <Profile />
+    </>
+    // <div classNameName="App">
+    //     <Navbar/>
+    //     <Section1/>
+    //     <Section2/>
+    //     <Section3/>
+    //     <Section4/>
+    //     <Section5/>
+    //     <Section6/>
+    //     <Footer/>
+    //     <Credits/>
+    // </div>
+
   );
 }
 
